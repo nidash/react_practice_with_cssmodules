925a8b2aa5203a8ba0a56ec385a8d75606ac6be9
Nidash <nidash@gmail.com>
2023-09-03 10:55:58 +0100

    Add validations

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import Card from '../UI/Card';
-import classes from './AddUsers.module.css';
-import Button from '../UI/Button';
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import classes from "./AddUsers.module.css";
+import Button from "../UI/Button";
 
-const AddUsers = props => {
-    const addUserHandler = (event) => {
-        event.preventDefault();
-        console.log("SUMIT");
+const AddUsers = (props) => {
+  const [username, setUsername] = useState("");
+  const [age, setAge] = useState("");
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    if (username.trim().length === 0 || age.trim().length === 0) {
+        return;
     }
-return (
+    if (+age < 1) {
+        return;
+    }
+    console.log("SUBMIT" + username, age);
+    setAge("");
+    setUsername("");
+  };
+
+  const usernameChangeHandler = (event) => {
+    setUsername(event.target.value);
+  };
+  const ageChangeHandler = (event) => {
+    setAge(event.target.value);
+  };
+
+  return (
     <Card className={classes.input}>
-    <form onSubmit={addUserHandler}>
+      <form onSubmit={addUserHandler}>
         <label htmlFor="username">User Name</label>
-        <input type="text" id="username" />
+        <input
+          type="text"
+          id="username"
+          onChange={usernameChangeHandler}
+          value={username}
+        />
         <label htmlFor="age">Age (Years)</label>
-        <input type="number" id="age" />
+        <input type="number" id="age" onChange={ageChangeHandler} value={age} />
         <Button type="submit">Add User</Button>
-    </form>
+      </form>
     </Card>
-)
-}
+  );
+};
 
 export default AddUsers;
