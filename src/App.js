7a8623dd9dad14b48754c621f39ffcce169be548
Nidash <nidash@gmail.com>
2023-09-03 11:05:00 +0100

    Add UsersList and dummy content.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import AddUsers from './components/Users/AddUsers';
+import UsersList from './components/Users/UsersList';
 
-
+const users = [
+  {name: 'Tito',  age: '15'},
+  {name: 'Titi',  age: '25'},
+];
+  
 function App() {
   return (
     <div>
       <AddUsers />
+      <UsersList users={users}/>
     </div>
   );
 }
